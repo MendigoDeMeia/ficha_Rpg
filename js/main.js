567baabb3a7c666fda8dcb85bd4ef3c174ff26de
ThatsTom <tomholanda300@gmail.com>
2022-07-04 09:19:58 -0300

    atualizando status corretamente

--- js/main.js
+++ js/main.js
@@ -49,9 +49,60 @@ const pecas = {
         "agilidade": 2,
         "poder": 1,
         "sorte": 10,
+    },
+
+    "forcaN": {
+        "forca": -5,
+        "defesa": -2,
+        "hp":-5,
+        "agilidade": -1,
+        "poder": 0,
+        "sorte": 0,
+    },
+    "defesaN": {
+        "forca": -2,
+        "defesa": -5,
+        "hp":5,
+        "agilidade": 0,
+        "poder": 0,
+        "sorte": 0,
+    },
+    "hpN": {
+        "forca": 0,
+        "defesa": -5,
+        "hp":-20,
+        "agilidade": 0,
+        "poder": 0,
+        "sorte": 0,
+    },
+    "agilidadeN": {
+        "forca": -1,
+        "defesa": 0,
+        "hp":-1,
+        "agilidade": -5,
+        "poder": -3,
+        "sorte": -5,
+    },
+    "poder/hakiN": {
+        "forca": -3,
+        "defesa": -2,
+        "hp":-2,
+        "agilidade": -2,
+        "poder": -5,
+        "sorte": -1,
+    },
+    "sorteN": {
+        "forca": 0,
+        "defesa": 0,
+        "hp":3,
+        "agilidade": 2,
+        "poder": 1,
+        "sorte": 10,
     }
 
 }
+
+
 
 controle.forEach((elemento) => {
     elemento.addEventListener('click', (evento) => {
@@ -66,6 +117,7 @@ function manipulaDados(operacao, controle) {
 
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1
+        
 
     }
     else {
@@ -80,6 +132,7 @@ function atualizaEstatistica(peca) {
         
     })
 }
+
 
 
 
