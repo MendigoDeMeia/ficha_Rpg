7d2966433e6baddaffd98dbf8f1292241b3fdef7
ThatsTom <tomholanda300@gmail.com>
2022-07-12 18:59:33 -0300

    adicionando inimigos no glossario

--- assets/js/statisticas_Personagem.js
+++ assets/js/statisticas_Personagem.js
@@ -1,8 +1,37 @@
 const ficha = {
-    "tompsom":{
-    "nome": "Tompsom",
-    "cargo": "Capitão dos Little Fly",
-    "recompensa": "$1.000.000.000,01",
-    "level": "30",
-    "status": 30 * 5
-}}
+    "David":{
+    "nome": "Bartolomeu D. Chester",
+    "akuma-no-mi": "Oto-Oto no mi",
+    "cargo": "Tripulante Daquela Cumbuquinha",
+    "recompensa": "$13.000 berry",
+    "level": 20,
+    "status": 60
+},
+"Nathan":{
+    "nome": "Jack o Caçador",
+    "akuma-no-mi": "Sem fruta",
+    "cargo": "Tripulante Daquela Cumbuquinha",
+    "recompensa": "$10.000 berry",
+    "level": 14,
+    "status": 42
+},
+"batata":{
+    "nome": "Lucas D. Zodiac",
+    "akuma-no-mi": "lino lino no mi",
+    "cargo": "Tripulante Daquela Cumbuquinha",
+    "recompensa": "$1.000 berry",
+    "level": 15,
+    "status": 45
+},
+"biel":{
+    "nome": "Sky Deep",
+    "akuma-no-mi": "sem fruta",
+    "cargo": "Capitão Daquela Cumbuquinha",
+    "recompensa": "$7.000 berry",
+    "level": 20,
+    "status": 60
+},
+
+}
+
+
